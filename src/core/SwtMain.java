10b3e16ca7b552ed37e19d7b83d07b6f56efda1b
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-02-22 08:54:21 -0600

    Prueba 1

--- src/core/SwtMain.java
+++ src/core/SwtMain.java
@@ -9,10 +9,12 @@ import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
 
 public class SwtMain {
 
-	protected Shell shell;
+	protected Shell shlConversorDeNmeros;
 	private Text txtNmeroAConvertir;
 
 	/**
@@ -34,9 +36,9 @@ public class SwtMain {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlConversorDeNmeros.open();
+		shlConversorDeNmeros.layout();
+		while (!shlConversorDeNmeros.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -47,24 +49,39 @@ public class SwtMain {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
-		shell.setLayout(new FormLayout());
+		shlConversorDeNmeros = new Shell();
+		shlConversorDeNmeros.setSize(450, 300);
+		shlConversorDeNmeros.setText("Conversor de N\u00FAmeros");
+		shlConversorDeNmeros.setLayout(new FormLayout());
 		
-		txtNmeroAConvertir = new Text(shell, SWT.BORDER);
+		txtNmeroAConvertir = new Text(shlConversorDeNmeros, SWT.BORDER);
 		FormData fd_txtNmeroAConvertir = new FormData();
 		fd_txtNmeroAConvertir.top = new FormAttachment(0, 10);
 		fd_txtNmeroAConvertir.left = new FormAttachment(0, 10);
 		txtNmeroAConvertir.setLayoutData(fd_txtNmeroAConvertir);
 		
-		Group grpSistemaDeEntrada = new Group(shell, SWT.NONE);
-		grpSistemaDeEntrada.setText("Sistema de Entrada");
-		grpSistemaDeEntrada.setLayout(new GridLayout(1, false));
-		FormData fd_grpSistemaDeEntrada = new FormData();
-		fd_grpSistemaDeEntrada.top = new FormAttachment(txtNmeroAConvertir, 0, SWT.TOP);
-		fd_grpSistemaDeEntrada.left = new FormAttachment(txtNmeroAConvertir, 6);
-		grpSistemaDeEntrada.setLayoutData(fd_grpSistemaDeEntrada);
+		Group grpConversin = new Group(shlConversorDeNmeros, SWT.NONE);
+		grpConversin.setText("Conversi\u00F3n");
+		grpConversin.setLayout(new GridLayout(1, false));
+		FormData fd_grpConversin = new FormData();
+		fd_grpConversin.top = new FormAttachment(txtNmeroAConvertir, 0, SWT.TOP);
+		fd_grpConversin.left = new FormAttachment(txtNmeroAConvertir, 6);
+		grpConversin.setLayoutData(fd_grpConversin);
+		
+		Button btnDecimalABinario = new Button(grpConversin, SWT.RADIO);
+		btnDecimalABinario.setText("Decimal a binario");
+		
+		Button btnDecimalAOctal = new Button(grpConversin, SWT.RADIO);
+		btnDecimalAOctal.setText("Decimal a octal");
+		
+		Button btnBinarioADecimal = new Button(grpConversin, SWT.RADIO);
+		btnBinarioADecimal.setText("Binario a decimal");
+		
+		Button btnOctalADecimal = new Button(grpConversin, SWT.RADIO);
+		btnOctalADecimal.setText("Octal a decimal");
+		
+		Button btnHexadecimalADecimal = new Button(grpConversin, SWT.RADIO);
+		btnHexadecimalADecimal.setText("Hexadecimal a Decimal");
 
 	}
 }
